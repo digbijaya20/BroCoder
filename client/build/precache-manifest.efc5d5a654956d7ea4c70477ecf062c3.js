347122e4b8e9b63bc9042abc33bc203873930c90
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2020-11-10 09:33:37 +0530

    Deploy

---
+++ client/build/precache-manifest.efc5d5a654956d7ea4c70477ecf062c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cc14d8edbcf41412e4199b36b59643",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05226010db1fd4f4cebd",
+    "url": "/static/css/main.ad2c80eb.chunk.css"
+  },
+  {
+    "revision": "f1d959b08e2a6da1bf2f",
+    "url": "/static/js/2.cba5e8a6.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.cba5e8a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05226010db1fd4f4cebd",
+    "url": "/static/js/main.919939a0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
